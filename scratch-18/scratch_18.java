f53f7b9ee818bdb32fffbf137d8a20d61089dacf
SeineWaurna <109640655+SeineWaurna@users.noreply.github.com>
2022-09-11 15:25:23 +0700

    pseudocode, Java, Text-Block for Printing 100-1

---
+++ scratch-18/scratch_18.java
@@ -0,0 +1,11 @@
+// Waruna S.
+// September 11, 2022
+// Printing 100 to 1
+
+class Scratch {
+    public static void main(String[] args) {
+        for(int n = 100; n>=1 ;n--){
+            System.out.print(n + " ");
+        }
+    }
+}
